64a43422fba3fe397f4d0661d3f56dbba39cc230
Md Mohsin Siddiqui <mdmohsinsiddiqui5@gmail.com>
2022-10-10 00:42:02 +0530

    updated treasury contracts

--- contracts/GovToken.sol
+++ contracts/GovToken.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import '@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol';
 
 contract Token is ERC20Votes {
     
